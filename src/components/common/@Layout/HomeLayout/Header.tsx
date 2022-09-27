347151619c86e67f8a5557a1c04788a0745afa57
roroism <miroiro0303@gmail.com>
2022-09-27 18:57:38 +0900

    feat: Create nav.

--- src/components/common/@Layout/HomeLayout/Header.tsx
+++ src/components/common/@Layout/HomeLayout/Header.tsx
@@ -1,26 +1,38 @@
-import { Container, Flex } from '@chakra-ui/react';
+import { Container, Flex, useDisclosure } from '@chakra-ui/react';
+
+import MobileNav from '@components/ModalsPage/MobileNav';
 
 import { LAYOUT } from '@constants/layout';
 
 import { BasicIcon, CartIcon } from 'generated/icons/MyIcons';
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Container px={0} as="header" w={{ base: '375px' }}>
-      <Flex
-        h={LAYOUT.HEADER.HEIGHT}
-        alignItems="center"
-        justifyContent="space-between"
-        position="fixed"
-        px="16px"
-        zIndex="999"
-        backgroundColor="yellow"
-        w="inherit"
-      >
-        <BasicIcon w="24px" h="24px" />
-        <CartIcon w="24px" h="24px" />
-      </Flex>
-    </Container>
+    <>
+      <Container px={0} as="header" w={{ base: '375px' }}>
+        <Flex
+          h={LAYOUT.HEADER.HEIGHT}
+          alignItems="center"
+          justifyContent="space-between"
+          position="fixed"
+          px="16px"
+          zIndex="999"
+          backgroundColor="yellow"
+          w="inherit"
+        >
+          <BasicIcon
+            w="24px"
+            h="24px"
+            _hover={{ cursor: 'pointer' }}
+            onClick={onOpen}
+          />
+          <CartIcon w="24px" h="24px" _hover={{ cursor: 'pointer' }} />
+        </Flex>
+      </Container>
+      <MobileNav isOpen={isOpen} onClose={onClose} size="sm" />
+    </>
   );
 };
 
--- src/generated/icons/MyIcons.tsx
+++ src/generated/icons/MyIcons.tsx
@@ -38,6 +38,38 @@ export const CartIcon = (props: IconProps) => (
     />
   </Icon>
 );
+export const LogoutIcon = (props: IconProps) => (
+  <Icon viewBox="0 0 24 24" {...props}>
+    <g clipPath="url(#clip0_5113_12900)">
+      <path
+        d="M16.3135 8.0625L20.2499 12L16.3135 15.9375"
+        stroke="#1A1A1A"
+        strokeWidth={1.3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.75 12H20.2472"
+        stroke="#1A1A1A"
+        strokeWidth={1.3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.75 20.25H4.5C4.30109 20.25 4.11032 20.171 3.96967 20.0303C3.82902 19.8897 3.75 19.6989 3.75 19.5V4.5C3.75 4.30109 3.82902 4.11032 3.96967 3.96967C4.11032 3.82902 4.30109 3.75 4.5 3.75H9.75"
+        stroke="#1A1A1A"
+        strokeWidth={1.3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_5113_12900">
+        <rect width={24} height={24} fill="white" />
+      </clipPath>
+    </defs>
+  </Icon>
+);
 export const MoonIcon = (props: IconProps) => (
   <Icon viewBox="0 0 24 24" {...props}>
     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />

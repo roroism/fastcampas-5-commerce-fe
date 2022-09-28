6cf13800f126b7c9af90ff7a4fab17c0d022e0ca
roroism <miroiro0303@gmail.com>
2022-09-28 23:54:29 +0900

    chore: change type.

---
+++ src/components/JoinPage/types.ts
@@ -0,0 +1,13 @@
+export type FormDataType = {
+  name: string;
+  nickname: string;
+  phone: string;
+  email: string;
+  gender?: string;
+  age?: string;
+  agreeAll: boolean;
+  agreePrivateInfo: boolean;
+  agreeRequired: boolean;
+  agreeMarketing: boolean;
+  ageRange: string;
+};

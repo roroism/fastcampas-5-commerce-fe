a025d088fa25b4d4738fb734d4357dc34ce1421b
roroism <miroiro0303@gmail.com>
2022-10-20 15:18:30 +0900

    fix: Check the existence of query value before executing logic.

--- src/components/PaymentSuccessPage/PaymentSuccessPage.tsx
+++ src/components/PaymentSuccessPage/PaymentSuccessPage.tsx
@@ -29,8 +29,98 @@ function PaymentSuccessPage({ ...basisProps }: PaymentSuccessPageProps) {
   //   variables: orderId,
   // });
 
+  // console.log('orderId : ', orderId);
+  // console.log('paymentKey : ', paymentKey);
+  // console.log('amount : ', amount);
+  // console.log('router.query.orderId : ', router.query.orderId);
+  // console.log('router.query.orderId : ', router.query.paymentKey);
+  // console.log('router.query.orderId : ', router.query.amount);
+  // productApi.getOrderByOrderId(orderId).then((res) => {
+  //   console.log(res);
+  //   const paymentByOrderIdData = res;
+
+  //   console.log('paymentByOrderIdData : ', paymentByOrderIdData);
+  //   console.log(
+  //     '가격 비교 Number(amount), paymentByOrderIdData?.amount : ',
+  //     Number(amount) === paymentByOrderIdData?.amount,
+  //     Number(amount),
+  //     paymentByOrderIdData?.amount,
+  //   );
+
+  //   if (
+  //     orderId &&
+  //     paymentKey &&
+  //     amount &&
+  //     typeof orderId === 'string' &&
+  //     typeof amount === 'string' &&
+  //     typeof paymentKey === 'string'
+  //   ) {
+  //     if (
+  //       paymentByOrderIdData
+  //       // &&
+  //       // Number(amount) === paymentByOrderIdData.price
+  //     ) {
+  //       const form = new FormData();
+  //       form.append('price', amount);
+  //       form.append('paymentKey', paymentKey);
+  //       form.append('method', paymentByOrderIdData?.method as string);
+  //       form.append('userName', paymentByOrderIdData?.userName as string);
+  //       form.append('userPhone', paymentByOrderIdData?.userPhone as string);
+  //       form.append(
+  //         'userAddrPost',
+  //         paymentByOrderIdData?.userAddrPost as string,
+  //       ); // 주소
+  //       form.append(
+  //         'userAddrDetail',
+  //         paymentByOrderIdData?.userAddrDetail as string,
+  //       ); // 주소
+  //       form.append('shipName', paymentByOrderIdData?.shipName as string); // 배송받을사람
+  //       form.append('shipPhone', paymentByOrderIdData?.shipPhone as string); // 배송연락처
+  //       form.append(
+  //         'shipAddrPost',
+  //         paymentByOrderIdData?.shipAddrPost as string,
+  //       ); // 배송지주소
+  //       form.append(
+  //         'shipAddrDetail',
+  //         paymentByOrderIdData?.shipAddrDetail as string,
+  //       ); // 배송지주소
+  //       form.append(
+  //         'orderMessage',
+  //         paymentByOrderIdData?.orderMessage as string,
+  //       ); // 배송요청사항
+
+  //       productApi
+  //         .putOrderByOrderId({
+  //           id: orderId,
+  //           data: form,
+  //         })
+  //         .then((res) => {
+  //           console.log('putOrderByOrderId res : ', res);
+
+  //           router.replace({
+  //             pathname: `/order/complete`,
+  //             query: { orderId: orderId },
+  //           });
+  //         });
+
+  //       // const PaymentAuthorizationCall = async () => {
+  //       //   const PaymentAuthorizationStatus = await axios.post(
+  //       //     'https://api.tosspayments.com/v1/payments/confirm',
+  //       //   );
+  //       // };
+  //     }
+  //   }
+  // });
+
   useEffect(() => {
+    if (!orderId || !paymentKey || !amount) return;
+
     console.log('orderId : ', orderId);
+    console.log('paymentKey : ', paymentKey);
+    console.log('amount : ', amount);
+    console.log('router.query.orderId : ', router.query.orderId);
+    console.log('router.query.orderId : ', router.query.paymentKey);
+    console.log('router.query.orderId : ', router.query.amount);
     productApi.getOrderByOrderId(orderId).then((res) => {
       console.log(res);
       const paymentByOrderIdData = res;
@@ -107,7 +197,7 @@ function PaymentSuccessPage({ ...basisProps }: PaymentSuccessPageProps) {
         }
       }
     });
-  }, [orderId]);
+  }, [orderId, router, amount, paymentKey]);
 
   return (
     <>

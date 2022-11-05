37726f21938edb1ec150a30df8b78d694548aab5
roroism <miroiro0303@gmail.com>
2022-11-05 22:39:37 +0900

    fix: Add scrollRestoration of next config.

--- src/components/ProductsPage/ProductsPage.tsx
+++ src/components/ProductsPage/ProductsPage.tsx
@@ -34,7 +34,7 @@ interface IProduct {
   reviewCount: number;
 }
 
-const cachedScrollPositions: number[][] = [];
+// const cachedScrollPositions: number[][] = [];
 
 interface ProductsPageProps extends ChakraProps {}
 
@@ -85,38 +85,47 @@ function ProductsPage({ ...basisProps }: ProductsPageProps) {
     },
   });
 
-  useEffect(() => {
-    if ('scrollRestoration' in window.history) {
-      window.history.scrollRestoration = 'auto';
-      let shouldScrollRestore: { x: number; y: number } | null;
+  // useEffect(() => {
+  //   if ('scrollRestoration' in window.history) {
+  //     window.history.scrollRestoration = 'auto';
+  //     let shouldScrollRestore: { x: number; y: number } | null;
 
-      Router.events.on('routeChangeStart', () => {
-        cachedScrollPositions.push([window.scrollX, window.scrollY]);
-      });
+  //     Router.events.on('routeChangeStart', () => {
+  //       console.log(
+  //         'routeChangeStart cachedScrollPositions: ',
+  //         cachedScrollPositions,
+  //       );
+  //       cachedScrollPositions.push([window.scrollX, window.scrollY]);
+  //     });
 
-      Router.events.on('routeChangeComplete', () => {
-        if (shouldScrollRestore) {
-          const { x, y } = shouldScrollRestore;
-          window.scrollTo(x, y);
-          shouldScrollRestore = null;
-        }
-        window.history.scrollRestoration = 'auto';
-      });
+  //     Router.events.on('routeChangeComplete', () => {
+  //       console.log(
+  //         'routeChangeComplete shouldScrollRestore : ',
+  //         shouldScrollRestore,
+  //       );
+  //       if (shouldScrollRestore) {
+  //         const { x, y } = shouldScrollRestore;
+  //         window.scrollTo(x, y);
+  //         shouldScrollRestore = null;
+  //       }
+  //       window.history.scrollRestoration = 'auto';
+  //     });
 
-      Router.beforePopState(() => {
-        if (cachedScrollPositions.length > 0) {
-          const [x, y]: [number, number] = cachedScrollPositions.pop() as [
-            number,
-            number,
-          ];
-          shouldScrollRestore = { x, y };
-        }
-        window.history.scrollRestoration = 'manual';
-        return true;
-      });
-    }
-  }, []);
-
+  //     Router.beforePopState(() => {
+  //       console.log('cachedScrollPositions : ', cachedScrollPositions);
+  //       if (cachedScrollPositions.length > 0) {
+  //         const [x, y]: [number, number] = cachedScrollPositions.pop() as [
+  //           number,
+  //           number,
+  //         ];
+  //         shouldScrollRestore = { x, y };
+  //       }
+  //       window.history.scrollRestoration = 'manual';
+  //       return true;
+  //     });
+  //   }
+  // }, []);
+  console.log('infinity data : ', data);
   return (
     <Box {...basisProps} px="16px" pt="120px" pb="80px" ref={boxRef}>
       <VisuallyHidden as="h2">main contents</VisuallyHidden>
